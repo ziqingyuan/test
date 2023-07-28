0e6df3a6d2c03f2d80fde3e74ad572ce73a22c62
Zoe Yuan <zoeziqingyuan@gmail.com>
2023-07-27 21:30:20 -0700

    func added: insert/update/count \n comments added

--- public/scripts.js
+++ public/scripts.js
@@ -1,42 +1,59 @@
+/*
+ * These functions below are for various webpage functionalities. 
+ * Each function serves to process data on the frontend:
+ *      - Before sending requests to the backend.
+ *      - After receiving responses from the backend.
+ * 
+ * Adjust or expand these functions to match your 
+ *   backend endpoints 
+ * and 
+ *   HTML structure 
+ * to tailor them to your specific needs.
+ */
+
+
+// This function checks the database connection and updates its status on the frontend.
 async function checkDbConnection() {
     const statusElem = document.getElementById('dbStatus');
     const loadingGifElem = document.getElementById('loadingGif');
 
-    const response = await fetch('/check-db-connection');
+    const response = await fetch('/check-db-connection', {
+        method: "GET"
+    });
 
+    // Hide the loading GIF once the response is received.
     loadingGifElem.style.display = 'none';
+    // Display the statusElem's text inline.
     statusElem.style.display = 'inline';
+
     response.text()
     .then((text) => {
         statusElem.textContent = text;
     })
     .catch((error) => {
-        statusElem.textContent = 'connection timed out';
+        statusElem.textContent = 'connection timed out';  // Adjust error handling if required.
     });
 }
 
-async function resetDemotable() {
-    const response = await fetch("/initiate-demotable", {
-        method: 'POST'
-    });
-    const responseData = await response.json();
-
-    if (responseData.success) {
-        const messageElement = document.getElementById('resetResult');
-        messageElement.textContent = "demotable initiated successfully!";
-        fetchTableData();
-    } else {
-        alert("Error initiating table!");
-    }
-}
-
+// Fetches data from the demotable and displays it.
 async function fetchAndDisplayUsers() {
-    const tableElement = document.getElementById('demoTable');
-    const response = await fetch('/users');
-    const users = await response.json();
-    
-    users.forEach(user => {
-        const row = tableElement.insertRow();
+    const tableElement = document.getElementById('demotable');
+    const tableBody = tableElement.querySelector('tbody');
+
+    const response = await fetch('/demotable', {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const demotableContent = responseData.data;
+
+    // Always clear old, already fetched data before new fetching process.
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    demotableContent.forEach(user => {
+        const row = tableBody.insertRow();
         user.forEach((field, index) => {
             const cell = row.insertCell(index);
             cell.textContent = field;
@@ -44,12 +61,113 @@ async function fetchAndDisplayUsers() {
     });
 }
 
+// This function resets or initializes the demotable.
+async function resetDemotable() {
+    const response = await fetch("/initiate-demotable", {
+        method: 'POST'
+    });
+    const responseData = await response.json();
+
+    if (responseData.success) {
+        const messageElement = document.getElementById('resetResultMsg');
+        messageElement.textContent = "demotable initiated successfully!";
+        fetchTableData();
+    } else {
+        alert("Error initiating table!");
+    }
+}
+
+// Inserts new records into the demotable.
+async function insertDemotable(event) {
+    event.preventDefault();
+
+    const idValue = document.getElementById('insertId').value;
+    const nameValue = document.getElementById('insertName').value;
+
+    const response = await fetch('/insert-demotable', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: idValue,
+            name: nameValue
+        })
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('insertResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = "Data inserted successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error inserting data!";
+    }
+}
+
+// Updates names in the demotable.
+async function updateNameDemotable(event) {
+    event.preventDefault();
+
+    const oldNameValue = document.getElementById('updateOldName').value;
+    const newNameValue = document.getElementById('updateNewName').value;
+
+    const response = await fetch('/update-name-demotable', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            oldName: oldNameValue,
+            newName: newNameValue
+        })
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('updateNameResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = "Name updated successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error updating name!";
+    }
+}
+
+// Counts rows in the demotable.
+// Modify the function accordingly if using different aggregate functions or procedures.
+async function countDemotable() {
+    const response = await fetch("/count-demotable", {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('countResultMsg');
+
+    if (responseData.success) {
+        const tupleCount = responseData.count;
+        messageElement.textContent = `The number of tuples in demotable: ${tupleCount}`;
+    } else {
+        alert("Error in count demotable!");
+    }
+}
+
+
+// ---------------------------------------------------------------
+// Initializes the webpage functionalities.
+// Add or remove event listeners based on the desired functionalities.
 window.onload = function() {
     checkDbConnection();
-    document.getElementById("resetDemotable").addEventListener("click", resetDemotable)
     fetchTableData();
+    document.getElementById("resetDemotable").addEventListener("click", resetDemotable);
+    document.getElementById("insertDemotable").addEventListener("submit", insertDemotable);
+    document.getElementById("updataNameDemotable").addEventListener("submit", updateNameDemotable);
+    document.getElementById("countDemotable").addEventListener("click", countDemotable);
 };
 
+// General function to refresh the displayed table data. 
+// You can invoke this after any table-modifying operation to keep consistency.
 function fetchTableData() {
     fetchAndDisplayUsers();
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const oracledb = require('oracledb');
 
-// These lines should be placed at the top of this file so the db config get loaded
+// Load environment variables from .env file
+// Ensure your .env file has the required database credentials.
 require('dotenv').config();
 
+// Database configuration setup. Ensure your .env file has the required database credentials.
 const dbConfig = {
     user: process.env.ORACLE_USER,
     password: process.env.ORACLE_PASS,
@@ -11,73 +13,23 @@ const dbConfig = {
 };
 
 const app = express();
-const PORT = 65535;
+const PORT = 65535;  // Adjust the PORT if needed (e.g., if you encounter a "port already occupied" error)
 
-app.use(express.static('public'));
+// Middleware setup
+app.use(express.static('public'));  // Serve static files from the 'public' directory
+app.use(express.json());             // Parse incoming JSON payloads
 
 
-async function testOracleConnection() {
-    let connection;
-  
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-      return true;
-    } catch (err) {
-      console.error(err);
-      return false;
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error(err);
-        }
-      }
-    }
-  }
-
-  async function initiateDemontable() {
-    let connection;
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-      try {
-        await connection.execute(`DROP TABLE DEMOTABLE`);
-      } catch(err) {
-        console.log('Table might not exist, proceeding to create...');
-      }
-
-      const result = await connection.execute(`
-        CREATE TABLE DEMOTABLE (
-            id NUMBER PRIMARY KEY,
-            name VARCHAR2(20)
-        )
-      `);
-        
-      console.log("Table created:", result);
-      return true;
-    } catch (err) {
-      console.error(err);
-      return false;
-    } finally {
-      if (connection) {
-          try {
-              await connection.close();
-          } catch (err) {
-              console.error(err);
-          }
-      }
-    }
-  }
-
-  async function fetchUserFromDb() {
+// ----------------------------------------------------------
+// Wrapper to manage OracleDB actions, simplifying connection handling.
+async function withOracleDB(action) {
     let connection;
     try {
         connection = await oracledb.getConnection(dbConfig);
-        const result = await connection.execute('SELECT * FROM DEMOTABLE');
-        return result.rows;
+        return await action(connection);
     } catch (err) {
         console.error(err);
-        return [];
+        throw err;
     } finally {
         if (connection) {
             try {
@@ -90,32 +42,148 @@ async function testOracleConnection() {
 }
 
 
-app.get('/check-db-connection', async (req, res) => {
-  const isConnect = await testOracleConnection();
+// ----------------------------------------------------------
+// Core functions for database operations
+// Modify these functions, especially the SQL queries, based on your project's requirements and design.
+async function testOracleConnection() {
+    return await withOracleDB(async (connection) => {
+        return true;
+    }).catch(() => {
+        return false;
+    });
+}
 
-  if (isConnect) {
-    res.send('connected');
-  } else {
-    res.send('unable to connect');
-  }
+async function fetchDemotableFromDb() {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute('SELECT * FROM DEMOTABLE');
+        return result.rows;
+    }).catch(() => {
+        return [];
+    });
+}
+
+async function initiateDemotable() {
+    return await withOracleDB(async (connection) => {
+        try {
+            await connection.execute(`DROP TABLE DEMOTABLE`);
+        } catch(err) {
+            console.log('Table might not exist, proceeding to create...');
+        }
+
+        const result = await connection.execute(`
+            CREATE TABLE DEMOTABLE (
+                id NUMBER PRIMARY KEY,
+                name VARCHAR2(20)
+            )
+        `);
+        return true;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function insertDemotable(id, name) {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute(
+            `INSERT INTO DEMOTABLE (id, name) VALUES (:id, :name)`,
+            [id, name],
+            { autoCommit: true }
+        );
+
+        return result.rowsAffected && result.rowsAffected > 0;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function updateNameDemotable(oldName, newName) {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute(
+            `UPDATE DEMOTABLE SET name=:newName where name=:oldName`,
+            [newName, oldName],
+            { autoCommit: true }
+        );
+
+        return result.rowsAffected && result.rowsAffected > 0;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function countDemotable() {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
+        return result.rows[0][0];
+    }).catch(() => {
+        return -1;
+    });
+}
+
+
+// ----------------------------------------------------------
+// API endpoints
+// Modify or extend these routes based on your project's needs.
+app.get('/check-db-connection', async (req, res) => {
+    const isConnect = await testOracleConnection();
+    if (isConnect) {
+        res.send('connected');
+    } else {
+        res.send('unable to connect');
+    }
+});
+
+app.get('/demotable', async (req, res) => {
+    const tableContent = await fetchDemotableFromDb();
+    res.json({data: tableContent});
 });
 
 app.post("/initiate-demotable", async (req, res) => {
-  const initiateResult = await initiateDemontable();
-
-  if (initiateResult) {
-    res.json({ success: true });
-  } else {
-    res.status(500).json({ success: false });
-  }
+    const initiateResult = await initiateDemotable();
+    if (initiateResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
 });
 
-app.get('/users', async (req, res) => {
-  const users = await fetchUserFromDb();
-  res.json(users);
+app.post("/insert-demotable", async (req, res) => {
+    const { id, name } = req.body;
+    const insertResult = await insertDemotable(id, name);
+    if (insertResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
+app.post("/update-name-demotable", async (req, res) => {
+    const { oldName, newName } = req.body;
+    const updateResult = await updateNameDemotable(oldName, newName);
+    if (updateResult) {
+        res.json({ success: true });
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
+app.get('/count-demotable', async (req, res) => {
+    const tableCount = await countDemotable();
+    if (tableCount >= 0) {
+        res.json({ 
+            success: true,  
+            count: tableCount
+        });
+    } else {
+        res.status(500).json({ 
+            success: false, 
+            count: tableCount
+        });
+    }
 });
 
 
+// ----------------------------------------------------------
+// Starting the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
